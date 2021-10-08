200b4ef6d02e494b0e80f80a981dc5bb3505a596
Snubb <oliver.lundqvist@elev.ga.ntig.se>
2021-10-08 10:51:35 +0200

    ✨ Gjort lite items och ändrade hur bilderna är hanterade

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const moment = require('moment');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css/');
     eleventyConfig.addWatchTarget('./src/css/');
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addWatchTarget("src/images");
+
+    eleventyConfig.addFilter('dateFilter', (value) =>{
+        moment.locale('sv');
+        return moment(value).format('LL');
+    }) 
 
     eleventyConfig.addPlugin(syntaxHighlight);
     return {
